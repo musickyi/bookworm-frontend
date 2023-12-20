3d2aa3f3ef4c381de8d7ff34a6fe6615c0f4f94e
yj0216 <yeojun0216@naver.com>
2023-12-20 18:24:47 +0900

    안쓰는 코드 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,7 @@
 import type { AppProps } from "next/app";
-import { useEffect, useState } from "react"; // useState 추가
-import { useRouter } from 'next/router';
 import { GlobalStyles } from '../styles/GlobalStyles';
-import LoginPage from "../components/LoginPage";
-import API from "../api";
-import useAuth from "../hooks/useAuth";
 import { Provider } from 'react-redux';
 import store from "../store/store";
-import { TokenType } from "../type/TokenType";
-
 
 const App = ({ Component, pageProps }: AppProps) => {
 
